fe6a0918aabeebdbbc388301123f8cc47232e147
Evangelopoulos <evangelopoulos@ckc.de>
2019-03-28 17:46:24 +0100

    add firebase backe end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { DataStorageService } from './shared/data-storage.service';
+
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
@@ -55,7 +57,7 @@ import { GrowlModule } from 'primeng/primeng';
     BrowserAnimationsModule ,
     GrowlModule 
   ],
-  providers: [ShoppingListService, RecipeService,AuthService, AuthGuard],
+  providers: [ShoppingListService, RecipeService,DataStorageService,AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -29,6 +29,10 @@ export class RecipeService {
 
   constructor(private slService: ShoppingListService) {}
 
+  setRecipes(recipes:Recipe[]){
+      this.recipes=recipes;
+      this.recipesChanged.next(this.recipes.slice());
+  }
   getRecipes() {
     return this.recipes.slice();
   }
